22cf66842d54c79d6216d56e380ff689efe1dfb7
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-06-07 17:15:39 +0200

    feat : add click event read array

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,6 +1,25 @@
+// creo array database mail list
+
+const mailList = ["irene@live.it" ,"marco@live.it","aldo@live.it","alice@live.it","giovanni@live.it","luca@live.it","paolo@live.it","marta@live.it"]
+console.log(mailList);
+
 // Chiedi all’utente la sua email, 
 
+const bottone = document.querySelector(".btn");
+const formMail = document.getElementById("mail");
+bottone.addEventListener("click",
+    function(){
+        let valoreMail = formMail.value;
+        console.log(valoreMail);
 
+        // controlla che sia nella lista, creo ciclo for
+        for (let i=0; i< mailList.length; i++){
+            let lista= mailList[i]
+            console.log(lista);
+        }
+
+    }
+)
 
 
 // controlla che sia nella lista di chi può accedere, 
