48a33c8b4f7e5b9b55e75b8562afb3bbd068c7f7
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-06-07 18:08:30 +0200

    fix cicle for

--- mail/js/main.js
+++ mail/js/main.js
@@ -4,7 +4,7 @@ const mailList = ["irene@live.it" ,"marco@live.it","aldo@live.it","alice@live.it
 console.log(mailList);
 
 // Chiedi all’utente la sua email, 
-
+let risultato;
 const bottone = document.querySelector(".btn");
 const formMail = document.getElementById("mail");
 const vero = document.querySelector(".si")
@@ -15,19 +15,23 @@ bottone.addEventListener("click",
         console.log(valoreMail);
 
         // controlla che sia nella lista, creo ciclo for
-        for (let i=0; i< mailList.length; i++){
+        for (let i=0; i < mailList.length; i++){
             let lista= mailList[i]
             console.log(lista);
-
             // controlla che sia nella lista di chi può accedere, 
             // stampa un messaggio appropriato sull’esito del controllo.
-            if( valoreMail=== lista){
-                vero.classList.add("display")
-            }else {
-                falso.classList.add("display")
-            }
-        }
+            if( valoreMail === lista){
+                 risultato = true
 
+            }
+            
+        }
+        
+        if(risultato === true){
+            vero.classList.add("display");
+        }else{
+            falso.classList.add("display");
+        }
     }
 )
 
