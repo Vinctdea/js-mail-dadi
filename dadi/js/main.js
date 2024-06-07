6628ec53710bf9e45dad1b59ba44046a8bb992da
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-06-07 22:39:07 +0200

    fine esercizio

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,7 +1,28 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 
+const btn = document.getElementById("btn")
+const risultato = document.querySelector(".risultato")
+const numeroHum = document.querySelector(".hum")
+const numeroBot = document.querySelector(".bot")
+btn.addEventListener("click",
+    function(){
+       let numeroUser = Math.trunc(6*Math.random()) + 1;
+       let numeroPc = Math.trunc(6*Math.random()) + 1;
+       console.log("il numero pc:"+ numeroPc, "il mio numero: " +numeroUser);   
+       numeroBot.innerHTML="il numero pc:"+ numeroPc;
+       numeroHum.innerHTML="il tuo numero:"+ numeroUser;
+     
+       if( numeroPc < numeroUser){
+        risultato.innerHTML="Hai vintooooo!!!!"
+       }else{
+        risultato.innerHTML="Hai perso!!!!"
+       }
 
+       console.log(risultato);
+      
+    }
 
+)
 
 
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
