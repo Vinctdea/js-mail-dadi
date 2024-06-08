30440919ff9c9ee1969e77da0ac781a7a6b8286d
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-06-08 02:40:42 +0200

    feat: add style dadi

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -8,14 +8,21 @@ btn.addEventListener("click",
     function(){
        let numeroUser = Math.trunc(6*Math.random()) + 1;
        let numeroPc = Math.trunc(6*Math.random()) + 1;
+       numeroHum.classList.remove("none");
+       numeroBot.classList.remove("none");
+
        console.log("il numero pc:"+ numeroPc, "il mio numero: " +numeroUser);   
-       numeroBot.innerHTML="il numero pc:"+ numeroPc;
-       numeroHum.innerHTML="il tuo numero:"+ numeroUser;
-     
+       numeroBot.innerHTML="numero pc: "+ numeroPc;
+       numeroHum.innerHTML="il tuo numero: "+ numeroUser;
+
+        // Stabilire il vincitore, in base a chi fa il punteggio più alto.
        if( numeroPc < numeroUser){
         risultato.innerHTML="Hai vintooooo!!!!"
+       }else if( numeroPc=== numeroUser){
+        risultato.innerHTML="c'è un pareggio!!!!"
        }else{
         risultato.innerHTML="Hai perso!!!!"
+
        }
 
        console.log(risultato);
@@ -25,4 +32,3 @@ btn.addEventListener("click",
 )
 
 
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
